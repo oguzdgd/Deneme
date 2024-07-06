fdad75a036ccf3412467cef6b7dd2fca40fca39b
Oğuzhan Doğdu <oguzhn.dgd@gmail.com>
2024-07-06 16:28:38 +0300

    ogi commit

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'jhdfjhsdk',
+              'bla bla ',
             ),
             Text(
               '$_counter',
